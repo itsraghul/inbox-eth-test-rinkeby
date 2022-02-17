892f35f91acc5e482d576ef4b5667e0c65091b56
Raghul S <raghul2521@gmail.com>
2022-02-17 14:55:25 +0530

    msg

---
+++ test/inbox.test.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts that have
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['hello boi'],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Inbox', () => {
+  it('it deploys a contract', () => {
+    console.log(inbox);
+  });
+});
+
+// class Car {
+//   park() {
+//     return 'stop';
+//   }
+
+//   drive() {
+//     return 'go';
+//   }
+// }
+
+// let car;
+// beforeEach(() =>
